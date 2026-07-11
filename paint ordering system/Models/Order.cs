88d0c675277e4ea2e35590daa7da834db96c6dcf
muzidianli-lw <muzidianli@aliyun.com>
2026-07-11 22:42:16 +0800

    add 4 funcs

--- paint ordering system/Models/Order.cs
+++ paint ordering system/Models/Order.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Dynamic;
+using System.Net.Http.Headers;
+using paint_ordering_system.Enums;
 
 namespace paint_ordering_system.Models;
 
@@ -10,13 +13,12 @@ public class Order
 
     public IReadOnlyList<OrderItem> Products => _products;
 
-    private decimal TotalPrice { get; }  //req
+    private decimal TotalPrice=>Math.Round(Products.Sum(p=>p.TotalPrice), 2, MidpointRounding.AwayFromZero);
 
     public Order(List<OrderItem> products) // req
     {
-        _products = products;
+        _products = products.ToList();
         _createdAt = DateTime.Now;
-        TotalPrice = Math.Round(Products.Sum(p=>p.TotalPrice), 2, MidpointRounding.AwayFromZero);
     }
 
     public void DisplayOrder() // req
@@ -30,5 +32,39 @@ public class Order
     public decimal GetTotalOrderPrice() // req
     {
         return TotalPrice;
+    }
+
+    public PaintProduct GetMostExpensivePaintProduct()
+    {
+        OrderItem? mostExpensiveOrderItm = _products.OrderBy(p=>p.TotalPrice).LastOrDefault();
+        if (mostExpensiveOrderItm == null)
+        {
+            throw new InvalidOperationException("The order contain no products");
+        }
+        return mostExpensiveOrderItm.Product;
+    }
+
+    public bool RemoveProduct(int productId)
+    {
+        OrderItem? removedOrderItm = _products.FirstOrDefault(p=>p.Product.Id == productId);
+        if(removedOrderItm == null)
+        {
+            return false;
+        }
+        return _products.Remove(removedOrderItm);
+    }
+
+    public List<OrderItem> GetPaintProductOfPriceBetween(decimal minPrice, decimal maxPrice)
+    {
+        if (minPrice >= maxPrice)
+        {
+            throw new ArgumentException("minPrice >= maxPrice");
+        }
+        return _products.Where(p=>(p.Product.GetFinalPrice() > minPrice) && (p.Product.GetFinalPrice() < maxPrice)).ToList();
+    }
+
+    public Dictionary<PaintType, decimal> GetTotalPriceForEachPaintType()
+    {
+        return _products.GroupBy(p=>p.Product.Type).ToDictionary(g=>g.Key, g=>g.Sum(p=>p.TotalPrice));
     }
 }
