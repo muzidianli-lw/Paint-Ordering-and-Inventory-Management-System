c8e2b882908287099fda2dafca71ad2538e76037
muzidianli-lw <muzidianli@aliyun.com>
2026-07-12 21:12:22 +0800

    add 2 enums

---
+++ paint ordering system/Enums/PaymentMethod.cs
@@ -0,0 +1,8 @@
+namespace paint_ordering_system.Enums;
+
+public enum PaymentMethod
+{
+    Alipay,
+    CreditCard,
+    BandTransfer
+}
---
+++ paint ordering system/Enums/PaymentState.cs
@@ -0,0 +1,8 @@
+namespace paint_ordering_system.Enums;
+
+public enum PaymentState
+{
+    Pending,
+    Failed,
+    Success
+}
