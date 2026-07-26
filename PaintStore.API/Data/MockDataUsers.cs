f38ab87c2dbff8d15d1f784ae60d2877bf20985a
muzidianli-lw <muzidianli@aliyun.com>
2026-07-26 17:03:54 +0800

    change user ctor

--- PaintStore.API/Data/MockDataUsers.cs
+++ PaintStore.API/Data/MockDataUsers.cs
@@ -6,8 +6,8 @@ namespace PaintStore.API.Data;
 public class MockDataUsers
 {
     public static List<User> Users = [
-        new User(0, "abc", "abc@aaaa", "1300000"),
-        new User(1, "def", "def@aaaa", "1312300"),
-        new User(2, "ghi", "ghi@aaaa", "1300012"),
+        new User("abc", "abc@aaaa", "1300000"),
+        new User("def", "def@aaaa", "1312300"),
+        new User("ghi", "ghi@aaaa", "1300012"),
     ];
 }
