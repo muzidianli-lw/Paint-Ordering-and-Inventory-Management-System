f47331f7d987baed34305ea506bdff4524f64ff8
muzidianli-lw <muzidianli@aliyun.com>
2026-08-10 00:21:59 +0800

    add database migrations

---
+++ PaintStore.API/Migrations/20260809161141_PaintProductProcess.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaintStore.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaintProductProcess : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "PaintProducts",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "PaintProducts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Inventory",
+                table: "PaintProducts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "PaintProducts",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaintProducts_Name",
+                table: "PaintProducts",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PaintProducts_Name",
+                table: "PaintProducts");
+
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "PaintProducts");
+
+            migrationBuilder.DropColumn(
+                name: "Inventory",
+                table: "PaintProducts");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "PaintProducts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "PaintProducts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
