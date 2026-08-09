a01c4ccf8a747913dcbd24e610aaaa4f031a7a6e
muzidianli-lw <muzidianli@aliyun.com>
2026-08-09 23:04:36 +0800

    add new member and validate

--- PaintStore.Model/PaintProduct.cs
+++ PaintStore.Model/PaintProduct.cs
@@ -6,24 +6,29 @@ public class PaintProduct
 {
     public int Id { get; private set;}
     public string Name { get; private set;} = "";
+    public string Brand { get; private set; } = "";
     public decimal Price { get; private set;}
+    public int Inventory { get; private set; }
     public DateTime CreatedAt {get; private init;}
+
+    public byte[] RowVersion { get; set; } = null!;
 
     private PaintProduct()
     {
         
     }
 
-    public PaintProduct(string name, decimal price)
+    public PaintProduct(string name, decimal price, string brand, int inventory)
     {
-        if (string.IsNullOrWhiteSpace(name))
-        {
-            throw new ArgumentException("name is null or whitespace");
-        }
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(brand);
         ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(price, 0);
+        ArgumentOutOfRangeException.ThrowIfLessThan(inventory, 1);
 
         Name = name.Trim();
+        Brand = brand.Trim();
         Price = price;
+        Inventory = inventory;
         CreatedAt = DateTime.UtcNow;
     }
 }
